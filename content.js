d77eb81de7658edb37f1dbe80689d5241f5f375b
徐舟骏 <xuzj@crunii.com>
2013-12-24 10:31:40 +0800

    1、增加重新加载图片前的延时 2、重新加载图片时使用随机参数以使本地缓存无效

--- content.js
+++ content.js
@@ -1,19 +1,42 @@
-var $el;
+var $el, seed = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+
+var getRandomNum = function() {
+    var num = '';
+    for (var i = 0; i < 16; i++) {
+        num += seed[Math.floor(Math.random() * 15)];
+    }
+    return num;
+};
+
+var parseUrl = function(url) {
+    if (url.indexOf('ImageReloaderRandomNum=') > -1) {
+        var temp = url.split('ImageReloaderRandomNum=');
+        url = temp[0] + 'ImageReloaderRandomNum=' + getRandomNum() + temp[1].substring(16);
+    } else if (url.indexOf('?') > -1) {
+        url += '&ImageReloaderRandomNum=' + getRandomNum();
+    } else {
+        url += '?ImageReloaderRandomNum=' + getRandomNum();
+    }
+
+    console.log(url);
+    return url;
+};
+
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     if (message.type == 'page') {
         $('img').each(function() {
-            var $t = $(this), url = $t.attr('src');
-            $t.attr('src', 'about:blank');
+            var $t = $(this), url = parseUrl($t.attr('src'));
+            $t.attr('src', chrome.extension.getURL("icon128.png"));
             setTimeout(function() {
                 $t.attr('src', url);
-            }, 10);
+            }, 500);
         });
     } else if (message.type == 'img') {
-        var url = $el.attr('src');
-        $el.attr('src', 'about:blank');
+        var url = parseUrl($el.attr('src'));
+        $el.attr('src', chrome.extension.getURL("icon128.png"));
         setTimeout(function() {
             $el.attr('src', url);
-        }, 10);
+        }, 500);
     }
 
     sendResponse({
